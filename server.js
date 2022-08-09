1a726a8f35fccce6c3c8b57723f8c836487285a9
Stephanie Segura <Steph@Stephanies-MBP.attlocal.net>
2022-08-09 15:47:40 -0500

    redirected home page

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ app.engine('jsx', require('express-react-views').createEngine())
 
 // ROUTES
 app.get('/', (req, res) => {
-  // res.send('Welcome to an awesome App about Breads')
+  res.send('Welcome to an awesome App about Breads')
   res.redirect('/breads')
-  console.log('Welcome to an awesome App about Breads')
+  // console.log('Welcome to an awesome App about Breads')
   
 })
 
